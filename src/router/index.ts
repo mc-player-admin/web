d4d9a06f763bad54ae85fae5598211ef1a1b5771
alongw <alongw@alongw.cn>
2023-06-10 00:21:15 +0800

    chore: 设置默认样式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
+import Home from '@/views/Home/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    }
     // {
     //   path: '/about',
     //   name: 'about',
---
+++ src/views/Home/index.vue
@@ -0,0 +1,7 @@
+<!-- <script lang="ts" setup></script> -->
+<template>
+  <h1>测试</h1>
+</template>
+<style lang="less" scoped>
+@import url(@/assets/style/base.less);
+</style>
