04a63790d6f0e31f13b39d150106dc46ac561c41
LiTengFei <ltfei@ltfei.cn>
2023-06-09 23:16:56 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  Hello World
+</template>
+
+<style lang="less" scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import HelloWorld from '../components/HelloWorld.vue'
+</script>
+
+<template>
+  <main>
+    <HelloWorld />
+    <router-link to="/about">about</router-link>
+  </main>
+</template>
