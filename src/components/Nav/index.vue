dd1da80bf568d806ba04424f412364148107a26e
alongw <alongw@alongw.cn>
2023-06-10 01:24:41 +0800

    feat(/components/nav): 增加用户登录按钮

--- src/components/Nav/index.vue
+++ src/components/Nav/index.vue
@@ -1,25 +1,43 @@
-<script lang="ts" setup>
-// code...
-</script>
+<script lang="ts" setup></script>
 
 <template>
   <div class="nav">
-    <!-- <div class="logo"></div> -->
-    <h1>MC-Player-Admin</h1>
+    <div class="title">
+      <!-- <div class="logo"></div> -->
+      <h1>MC-Player-Admin</h1>
+    </div>
+    <div class="loginstatus">
+      <a href="javascript:;" v-if="true">您好，请登录</a>
+      <a href="javascript:;" v-else>Admin</a>
+    </div>
   </div>
 </template>
 
 <style lang="less" scoped>
 @import url(@/assets/style/base.less);
 .nav {
-  //   background-color: pink;
-  padding-left: 25px;
-  height: 60px;
   display: flex;
+  justify-content: space-between;
   align-items: center;
   box-shadow: 1px 1px 2px 1px #f2f2f2;
-  h1 {
-    color: #7764b5;
+  height: 60px;
+  padding: 0 25px;
+  .title {
+    display: flex;
+    align-items: center;
+    h1 {
+      color: #7764b5;
+    }
+  }
+
+  .loginstatus {
+    cursor: pointer;
+    a {
+      color: #409eff;
+      &:hover {
+        color: #ffbfc9;
+      }
+    }
   }
 }
 </style>
