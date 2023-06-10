35a3d355101fc804191fe9c454b84b5ccd92b679
alongw <alongw@alongw.cn>
2023-06-10 14:20:13 +0800

    style: 修复验证码类型默认选中不显示

--- src/pages/Login/LoginMethod/WithCode.vue
+++ src/pages/Login/LoginMethod/WithCode.vue
@@ -1,15 +1,15 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 
-const mode = ref(1)
+const mode = ref('bot')
 </script>
 
 <template>
   <div class="code">
     <div class="mb-2 flex items-center text-sm">
       <el-radio-group v-model="mode" class="ml-4">
-        <el-radio label="1" size="large">机器人验证</el-radio>
-        <el-radio label="2" size="large">邮件验证</el-radio>
+        <el-radio label="bot" size="large" aria-checked>机器人验证</el-radio>
+        <el-radio label="mail" size="large">邮件验证</el-radio>
       </el-radio-group>
     </div>
     <!-- 验证方式 -->
