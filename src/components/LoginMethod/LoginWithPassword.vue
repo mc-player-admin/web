ecdaae46a5badb5c0a876a014991df457876984c
ltfei <ltfei@ltfei.cn>
2023-06-15 10:48:19 +0800

    refactor(pages/Login): 修改登录页面格式

---
+++ src/components/LoginMethod/LoginWithPassword.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineOptions({
+  name: 'LoginWithPassword'
+})
+
+const form = ref({
+  user: '',
+  password: ''
+})
+</script>
+
+<template>
+  <el-form label-position="left" label-width="40px" :model="form">
+    <el-form-item label="QQ">
+      <el-input v-model="form.user" name="qq"></el-input>
+    </el-form-item>
+    <el-form-item label="密码">
+      <el-input v-model="form.password" name="password" show-password />
+    </el-form-item>
+  </el-form>
+</template>
+
+<style lang="less" scoped></style>
--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -1,57 +1,115 @@
 <script lang="ts" setup>
+import { ref, defineAsyncComponent } from 'vue'
+
 defineOptions({
   name: 'LoginPage'
 })
 
 const routers = [
   {
-    index: 'bypassword',
-    text: '密码登录'
+    index: 'password',
+    text: '密码登录',
+    components: defineAsyncComponent(() => import('@/components/LoginMethod/LoginWithPassword.vue'))
   },
   {
-    index: 'bycode',
-    text: '验证码登录'
+    index: 'code',
+    text: '验证码登录',
+    components: defineAsyncComponent(() => import('@/components/LoginMethod/LoginWithCode.vue'))
   },
   {
-    index: 'byapp',
+    index: 'qrcode',
     text: '扫码登录',
     disabled: true
   }
 ]
+
+const activeName = ref(routers[0].index)
 </script>
+
 <template>
   <div class="login">
-    <div class="login-title">
-      <h1>登录</h1>
-    </div>
+    <h1 class="login_title">登录</h1>
+
     <div class="login-mode-selete">
-      <el-menu default-active="bypassword" mode="horizontal" router>
-        <el-menu-item
+      <el-tabs v-model="activeName" class="demo-tabs">
+        <el-tab-pane
           v-for="item in routers"
           :key="item.index"
-          :index="item.index"
+          :label="item.text"
+          :name="item.index"
           :disabled="item.disabled"
         >
-          {{ item.text }}
-        </el-menu-item>
-      </el-menu>
+          <component v-if="item.components" :is="item.components" />
+        </el-tab-pane>
+      </el-tabs>
     </div>
-    <RouterView />
+    <div class="any_method">
+      <h3 class="title">其他方式登录</h3>
+      <div class="methods">
+        <div class="item qq">
+          <div class="icon">
+            <img src="@/assets/03_qq_symbol.png" alt="qq登录" />
+          </div>
+          QQ登录
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="less" scoped>
+// 居中
+:deep(.el-tabs__nav-scroll) {
+  display: flex;
+  justify-content: center;
+}
+:deep(.el-tabs__content) {
+  margin: 0 20px;
+  margin-top: 20px;
+}
 .login {
   margin: 0 auto;
-  max-width: 600px;
-  .login-title {
+  max-width: 550px;
+  .login_title {
     text-align: center;
-    font-size: 18px;
+    font-size: 26px;
     margin: 25px;
   }
 
   .login-mode-selete {
     margin: 25px;
+  }
+}
+
+.any_method {
+  .title {
+    text-align: center;
+    font-size: 16px;
+    font-weight: 400;
+  }
+  .methods {
+    display: flex;
+    justify-content: space-around;
+    .item {
+      display: flex;
+      align-items: center;
+      .icon {
+        width: 25px;
+        height: 25px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 8px;
+        margin-right: 5px;
+        cursor: pointer;
+      }
+      &.qq .icon {
+        background-color: #23a0f0;
+        img {
+          height: 18px;
+        }
+      }
+    }
   }
 }
 </style>
