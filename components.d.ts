f489da6660aac681c99c8c256fb0efc5e2df1341
ltfei <ltfei@ltfei.cn>
2023-06-16 15:33:10 +0800

    chore: dev服务器https模式

--- components.d.ts
+++ components.d.ts
@@ -9,11 +9,6 @@ declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCard: typeof import('element-plus/es')['ElCard']
-    ElForm: typeof import('element-plus/es')['ElForm']
-    ElFormItem: typeof import('element-plus/es')['ElFormItem']
-    ElInput: typeof import('element-plus/es')['ElInput']
-    ElTabPane: typeof import('element-plus/es')['ElTabPane']
-    ElTabs: typeof import('element-plus/es')['ElTabs']
     LoginWithCode: typeof import('./src/components/LoginMethod/LoginWithCode.vue')['default']
     LoginWithPassword: typeof import('./src/components/LoginMethod/LoginWithPassword.vue')['default']
     Nav: typeof import('./src/components/Nav/Nav.vue')['default']
