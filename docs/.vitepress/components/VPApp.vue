5929b2be8f6d18d17a59d837e00994e1d47d1e26
ltfei <ltfei@ltfei.cn>
2023-07-02 19:08:26 +0800

    feat(docs): 隐藏控件

--- docs/.vitepress/components/VPApp.vue
+++ docs/.vitepress/components/VPApp.vue
@@ -7,16 +7,23 @@ import { useSidebar } from '../composables/sidebar'
 const { Layout } = DefaultTheme
 const { open, useListenResize } = useSidebar()
 useListenResize()
+
+const controls = !location.search.includes('controls=false')
 </script>
 
 <template>
-  <div id="vp-app">
-    <VPSidebar />
-    <VPNav />
-    <div class="content vp-doc" :class="open ? 'open' : ''">
-      <Content />
+  <template v-if="controls">
+    <div id="vp-app">
+      <VPSidebar />
+      <VPNav />
+      <div class="content vp-doc" :class="open ? 'open' : ''">
+        <Content />
+      </div>
     </div>
-  </div>
+  </template>
+  <template v-else>
+    <Content class="vp-doc" />
+  </template>
 </template>
 
 <style lang="less" scoped>
@@ -37,30 +44,4 @@ useListenResize()
     overflow: auto;
   }
 }
-// #vp-app {
-//   display: flex;
-//   flex-direction: column;
-//   height: 100vh;
-//   overflow: hidden;
-//   .content {
-//     flex: 1;
-//     min-height: 0;
-//     overflow: auto;
-//   }
-//   .sidebar,
-//   .content {
-//     overflow: auto;
-//     min-height: 0;
-//   }
-//   .content {
-//     flex: 1;
-//     padding: 10px 32px 100px;
-//     box-sizing: border-box;
-//     transition: all 0.5s;
-//     &.open {
-//       margin-left: @sidebar-width;
-//     }
-//   }
-//   // }
-// }
 </style>
